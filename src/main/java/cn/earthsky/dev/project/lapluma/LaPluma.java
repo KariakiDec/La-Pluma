96f81f213051414bb986a6b086b7286b70880cb9
KariakiDec <m@kariaki.moe>
2022-11-03 11:27:26 +0800

    v1.0.0 release

--- src/main/java/cn/earthsky/dev/project/lapluma/LaPluma.java
+++ src/main/java/cn/earthsky/dev/project/lapluma/LaPluma.java
@@ -81,7 +81,6 @@ public class LaPluma {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         ClientCommandHandler.instance.registerCommand(new PlayJournalCommand());
-//        KeyLoader.init();
     }
 
     /**
